7625f71aa2308e314e2c23e270ff23c7d1615add
Zyanide1504 <42516755+Zyanide1504@users.noreply.github.com>
2020-10-04 22:05:53 +0700

    add personal_sup_ans

---
+++ dseg8/personal_sup_ans.py
@@ -0,0 +1,10 @@
+
+# with open('personal_sup_q.json', 'rb') as answer:
+#     _answer = pickle.load(answer)
+
+
+# def Get_Personal_sup_ans():
+    
+#     answer_list = list(set())
+
+#     return answer_list
