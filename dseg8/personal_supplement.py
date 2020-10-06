d14ed0de342025586767ef85e972a5ab194ffc01
Praphan Oranphanlert <e23thr@gmail.com>
2020-10-06 21:21:32 +0700

    update personal supplement api  (called from LIFF)

--- dseg8/personal_supplement.py
+++ dseg8/personal_supplement.py
@@ -8,17 +8,19 @@ class PersonalSupplement(Resource):
     def post(self):
         data = json.loads(request.get_data(as_text=True))
         ps = GoogleSheet(SHEET_PERSONAL_SUPPLEMENTS)
-        print("data", data)
+        # print("data", data)
         existing_row = ps.df.loc[ps.df.lineId == data['lineId']]
-        print('existing_row', existing_row)
+        # print('existing_row', existing_row)
         if len(existing_row) == 0:
-            print("Create a new row")
+            # print("Create a new row")
             ps.df = ps.df.append(data, ignore_index=True)
         else:
-            print("Update row")
+            # print("Update row")
             for k in data.keys():
-                existing_row[k] = data[k]
+                # print("Update key ", k, data[k])
+                ps.df.loc[ps.df.lineId == data['lineId'], k] = data[k]
             # ps.df.loc[ps.df.lineId == data.lineId] =
-        ps.df.fillna("")
+        ps.df = ps.df.fillna("")
+        # print(ps.df)
         ps.save()
         return data
