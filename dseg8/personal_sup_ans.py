d63f377eddb38209cd7cf9af4c912c2365c981b4
Praphan Oranphanlert <e23thr@gmail.com>
2020-10-14 14:47:20 +0700

    test api

--- dseg8/personal_sup_ans.py
+++ dseg8/personal_sup_ans.py
@@ -10,20 +10,23 @@ def Get_Personal_sup_ans(lineID):
 
     ans_file = open('personal_sup_q.json', 'rb')
     _answer = json.load(ans_file)
-    ans_data = GoogleSheet(SHEET_PERSONAL_SUPPLEMENTS).df.reset_index()
-  
+    ps = GoogleSheet(SHEET_PERSONAL_SUPPLEMENTS)
+    ans_data = ps.df.loc[ps.df.lineId == lineID]
+
+    if len(ans_data) == 0:
+        raise Exception("Cannot find lineID {}".format(lineId))
     ## MapAge
     if(int(ans_data.iloc[0]["อายุ"])>=40 and ans_data.iloc[0]['เพศ']=="ชาย"):
         map_age=["วิตามินบีรวม","โสมทะเลทราย"]
     elif(int(ans_data.iloc[0]["อายุ"])>=40 and ans_data.iloc[0]['เพศ']=="หญิง"):
-        map_age=[]    
+        map_age=[]
     ## Suncream
     if(str(ans_data.iloc[0]["การตากแดด"])=="มากกว่า 20 นาที"):
         if(ans_data.iloc[0]['ใช้ครีมกันแดด']=="ไม่ได้ทาเลย"):
            map_sunCream=["วิตามินบีรวม","โสมทะเลทราย"]
         elif(ans_data.iloc[0]['ใช้ครีมกันแดด']=="ไม่ได้ทาเลย" or ans_data.iloc[0]["ใช้ครีมกันแดด"]=="ไม่พบข้อมูล"):
-           map_sunCream=[]    
-    
+           map_sunCream=[]
+
 
     #Map Ans
     answer_list = list(set(
@@ -38,9 +41,9 @@ def Get_Personal_sup_ans(lineID):
         _answer["คำแนะนำเรื่องการเสริมธาตุเหล็กจากคุณหมอ"][ans_data.iloc[0]["คำแนะนำเรื่องการเสริมธาตุเหล็กจากคุณหมอ"]]+
         _answer["อยากดูแลด้านไหนเป็นพิเศษ"][ans_data.iloc[0]["special1"]]+
         _answer["อยากดูแลด้านไหนเป็นพิเศษ"][ans_data.iloc[0]["special2"]]+
-        _answer["อยากดูแลด้านไหนเป็นพิเศษ"][ans_data.iloc[0]["special3"]]        
+        _answer["อยากดูแลด้านไหนเป็นพิเศษ"][ans_data.iloc[0]["special3"]]
     ))
-    
+
     return answer_list
 
 # print(Get_Personal_sup_ans("U90eedeb88d0c990cfa053c9c19a6d97cU90eedeb88d0c990cfa053c9c19a6d97c"))
